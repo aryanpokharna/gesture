cdb10f6f5f72d4351d965bde582b06a259559a40
Dylan Godwin <dgod3239@uni.sydney.edu.au>
2022-04-12 16:34:23 +1000

    key gen

--- static/js/key.js
+++ static/js/key.js
@@ -1,17 +1,31 @@
 
 //const crypto = require("crypto");
-//import { generateKeyPairSync } from "crypto"
+//const { generateKeyPairSync } = require('crypto');
 //import crypto from 'crypto';
-const nacl = require('tweetnacl');
-nacl.util = require('tweetnacl-util');
+//const nacl = require('tweetnacl');
+//nacl.util = require('tweetnacl-util');
 
-const user = nacl.box.keyPair();
+//const user = nacl.box.keyPair();
 // The `generateKeyPairSync` method accepts two arguments:
 // 1. The type of keys we want, which in this case is "rsa"
 // 2. An object with the properties of the key
-// const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+//const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
 // 	// The standard secure default length for RSA keys is 2048 bits
-// 	modulusLength: 2048,
-// });
-console.log(user.publicKey, user.secretKey);
+	//modulusLength: 2048,
+     //});
 
+// const keyPair = window.crypto.subtle.generateKey(
+//     {
+//         name: "ECDH",
+//         namedCurve: "P-256",
+//       },
+//       true,
+//       ["deriveKey", "deriveBits"]
+//     );
+
+const pki = require('node-forge').pki;
+var keys = pki.rsa.generateKeyPair(2048);
+//needs to be heavily edited still but somewhat works 
+console.log(keys.publicKey);
+var pub = pki.publicKeyToPem(keys.publicKey, 72)
+console.log(pub)
